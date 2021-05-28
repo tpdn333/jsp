cd4204d5ca4d30bd3b0747be3b2e0a79a6661f1b
tpdn333 <tpdn333@gmail.com>
2021-05-28 15:43:51 +0900

    sql File

---
+++ WebContent/WEB-INF/sql/SELECT04.sql
@@ -0,0 +1,18 @@
+SELECT CustomerName FROM Customers
+WHERE CustomerID = 1;
+
+SELECT CustomerName, City
+FROM Customers
+WHERE CustomerID = 1;
+
+SELECT CustomerID, CustomerName, City
+FROM Customers
+WHERE CustomerID = 2;
+
+SELECT * FROM Employees
+WHERE EmployeeID = 1;
+
+SELECT DISTINCT City FROM Customers
+ORDER BY City;
+
+SELECT * FROM Employees;
