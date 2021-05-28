7c737c45152d44b0625c7b9ac40d396f4ec2e0e1
tpdn333 <tpdn333@gmail.com>
2021-05-28 15:43:22 +0900

    javaBean

---
+++ src/ch14/bean/Employee.java
@@ -0,0 +1,32 @@
+package ch14.bean;
+
+public class Employee {
+	private String id;
+	private String lastName;
+	private String firstName;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+}
