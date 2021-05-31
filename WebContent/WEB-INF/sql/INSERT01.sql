8599c355ef8d86e4cc8b43c70bf33c620242744c
tpdn333 <tpdn333@gmail.com>
2021-05-31 17:29:25 +0900

    sql FIle

--- WebContent/WEB-INF/sql/INSERT01.sql
+++ WebContent/WEB-INF/sql/INSERT01.sql
@@ -10,3 +10,20 @@ INSERT INTO Customers
 VALUE
 (93, 'Choi', 'Hong', 'Gangnam', 'Seoul', '00001', 'Korea');
 -- column값 안쓰고 value값 넣기
+
+INSERT INTO Customers
+(CustomerID, CustomerName, ContactName)
+VALUE
+(94, 'Shrimp', 'Son');
+-- 값을 안넣은건 NULL
+
+INSERT INTO Customers
+(CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUE
+('Trump', 'Donald', 'NY', 'NY', '00002',' US');
+
+-- 테이블 설명 보기
+DESC Customers;
+DESC Employees;
+
+SELECT * FROM Employees;
