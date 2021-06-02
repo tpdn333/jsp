417da215ad33f32e6eba12be7396ab9f3abf66db
tpdn333 <tpdn333@gmail.com>
2021-06-02 16:49:02 +0900

    MemberDAO

--- src/sample2/dao/MemberDAO.java
+++ src/sample2/dao/MemberDAO.java
@@ -3,7 +3,12 @@ package sample2.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 import sample2.bean.Member;
 
@@ -16,20 +21,26 @@ public class MemberDAO {
 		this.url = "jdbc:mysql://3.36.92.67/test_database";
 		this.user = "root";
 		this.password = "wnddkdwjdqhcjfl1";
+		
+		// 클래스 로딩
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public boolean insert(Member member) {
 		String sql = "INSERT INTO Member " +
-					 "(id, password, name, birth, inserted) " +
+					 "(id, password, name, birth, gender, inserted) " +
 					 "VALUES " +
-					 "(?, ?, ?, ?, NOW()) ";
+					 "(?, ?, ?, ?, ?, NOW()) ";
 		
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		
 		try {
-			// 클래스 로딩
-			Class.forName("com.mysql.cj.jdbc.Driver");
 			// mysql 연결
 			con = DriverManager.getConnection(url, user, password);
 			pstmt = con.prepareStatement(sql);
@@ -38,6 +49,7 @@ public class MemberDAO {
 			pstmt.setString(2, member.getPassword());
 			pstmt.setString(3, member.getName());
 			pstmt.setDate(4, member.getBirth());
+			pstmt.setString(5, member.getGender());
 			
 			int cnt = pstmt.executeUpdate();
 			if( cnt == 1) {
@@ -65,6 +77,73 @@ public class MemberDAO {
 		}
 			
 		return false;
+	}
+
+	public List<Member> list() {
+		List<Member> list = new ArrayList<Member>();
+		
+		String sql = "SELECT id, password, name, birth, gender, inserted FROM Member";
+		
+		try(
+				Connection con = DriverManager.getConnection(url, user, password);
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);
+				) {
+			while(rs.next()) {
+				Member member = new Member();
+				member.setId(rs.getString(1));
+				member.setPassword(rs.getString(2));
+				member.setName(rs.getString(3));
+				member.setBirth(rs.getDate(4));
+				member.setGender(rs.getString(5));
+				member.setInserted(rs.getTimestamp(6));
+				
+				list.add(member);
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public Member getMember(String id) {
+		String sql = "SELECT id, password, name, birth, gender, inserted " + 
+					 "FROM Member " +
+					 "WHERE id = ? ";
+		ResultSet rs = null;
+		try(
+			Connection con = DriverManager.getConnection(url, user, password);
+			PreparedStatement pstmt = con.prepareStatement(sql);
+				) {
+			pstmt.setString(1, id);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				Member member = new Member();
+				member.setId(rs.getString(1));
+				member.setPassword(rs.getString(2));
+				member.setName(rs.getString(3));
+				member.setBirth(rs.getDate(4));
+				member.setGender(rs.getString(5));
+				member.setInserted(rs.getTimestamp(6));
+				
+				return member;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
 	}
 
 }
