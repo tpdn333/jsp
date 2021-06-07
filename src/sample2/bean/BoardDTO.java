df867100975975ae833a481e4bf124e8452c1ec5
tpdn333 <tpdn333@gmail.com>
2021-06-07 17:50:48 +0900

    Sample bean

---
+++ src/sample2/bean/BoardDTO.java
@@ -0,0 +1,72 @@
+package sample2.bean;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+public class BoardDTO {
+	private int boardId;
+	private String title;
+	private String body;
+	private String memberName;
+	private Timestamp inserted;
+
+	public String getTimeAgo() {
+		long now = System.currentTimeMillis();
+		long inserted = this.inserted.getTime();
+		long diff = (now - inserted) / 1000;
+		String msg = "";
+		if(diff < 60) {
+			msg = diff + "초 전";
+		} else if((diff /= 60) < 60) {
+			msg = diff + "분 전";
+		} else if((diff /= 60) < 24) {
+			msg = diff + "시간 전";
+		} else if((diff /= 24) < 30) {
+			msg = diff + "일 전";
+		} else {
+			msg = new SimpleDateFormat("yy/MM/dd").format(this.inserted);
+		}
+		return msg;
+	}
+	
+	public int getBoardId() {
+		return boardId;
+	}
+
+	public void setBoardId(int boardId) {
+		this.boardId = boardId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public String getMemberName() {
+		return memberName;
+	}
+
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+
+	public Timestamp getInserted() {
+		return inserted;
+	}
+
+	public void setInserted(Timestamp inserted) {
+		this.inserted = inserted;
+	}
+
+}
---
+++ src/sample2/bean/SendEmail.java
@@ -0,0 +1,48 @@
+package sample2.bean;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.Properties;
+
+public class SendEmail {
+	public static void sendEmail(String email) {
+
+		String host = "smtp.naver.com";
+		final String user = "sztpdnzs@naver.com";
+		final String password = "thstpdn12@";
+
+		String to = email;
+
+		// Get the session object
+		Properties props = new Properties();
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.auth", "true");
+
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(user, password);
+			}
+		});
+
+		// Compose the message
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(user));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+			// Subject
+			message.setSubject("[Subject] Java Mail Test");
+
+			// Text
+			message.setText("Simple mail test..");
+
+			// send the message
+			Transport.send(message);
+			System.out.println("message sent successfully...");
+
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+	}
+}
