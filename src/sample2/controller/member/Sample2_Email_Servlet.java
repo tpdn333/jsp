1647d873cd855f73d2b335badd233f53e9c0c42a
tpdn333 <tpdn333@gmail.com>
2021-06-07 17:51:07 +0900

    SendEmail Servlet

---
+++ src/sample2/controller/member/Sample2_Email_Servlet.java
@@ -0,0 +1,41 @@
+package sample2.controller.member;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import sample2.bean.SendEmail;
+/**
+ * Servlet implementation class Sample2_Email_Servlet
+ */
+@WebServlet("/sample2/member/email")
+public class Sample2_Email_Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Sample2_Email_Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		SendEmail.sendEmail(email);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
