6a558796e87acee2ec5c1ed50ba66d10ff9c06e5
tpdn333 <tpdn333@gmail.com>
2021-06-08 19:26:19 +0900

    MemberDAO

--- src/sample2/dao/MemberDAO.java
+++ src/sample2/dao/MemberDAO.java
@@ -188,11 +188,11 @@ public class MemberDAO {
 		return false;
 	}
 
-	public void remove(String id) {
+	public void remove(String id, Connection con) {
 		String sql = "DELETE FROM Member WHERE id = ?";
 
-		try (Connection con = DriverManager.getConnection(url, user, password);
-				PreparedStatement pstmt = con.prepareStatement(sql);) {
+		try (
+			PreparedStatement pstmt = con.prepareStatement(sql);) {
 
 			pstmt.setString(1, id);
 			pstmt.executeUpdate();
