38362e5248d083ad0b7329b9167b1461cf89fb2b
tpdn333 <tpdn333@gmail.com>
2021-06-09 16:07:56 +0900

    board detail servlet

--- src/sample2/controller/board/Sample2_Board_Detail_Servlet.java
+++ src/sample2/controller/board/Sample2_Board_Detail_Servlet.java
@@ -1,6 +1,7 @@
 package sample2.controller.board;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,7 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import sample2.bean.BoardDTO;
+import sample2.bean.Comment;
 import sample2.dao.BoardDAO;
+import sample2.service.comment.CommentService;
 
 
 /**
@@ -18,6 +21,7 @@ import sample2.dao.BoardDAO;
 @WebServlet("/sample2/board/detail")
 public class Sample2_Board_Detail_Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static CommentService commentService = null;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -26,21 +30,31 @@ public class Sample2_Board_Detail_Servlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
+    
+    @Override
+    public void init() throws ServletException {
+    	// TODO Auto-generated method stub
+    	super.init();
+    	this.commentService = new CommentService();
+    }
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String id = request.getParameter("boardId");
+		String boardId = request.getParameter("boardId");
 		
-		if(id == null) {
+		if(boardId == null) {
 			String path = request.getContextPath() + "/sample2/board/detail";
 			response.sendRedirect(path);
 		} else {
 			BoardDAO dao = new BoardDAO();
-			BoardDTO board = dao.get2(Integer.parseInt(id));
+			BoardDTO board = dao.get2(Integer.parseInt(boardId));
 			
+			List<Comment> commentList = commentService.list(Integer.parseInt(boardId));
 			request.setAttribute("boards", board);
+			request.setAttribute("comments", commentList);
+			
 			String path = "/WEB-INF/sample2/board/detail.jsp";
 			request.getRequestDispatcher(path).forward(request, response);
 		}
